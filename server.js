3917f3f843ad4009237920e6089bf4afe1df108e
Ritu Gatiyala <ritu17gatiyala@gmail.com>
2017-02-25 01:36:05 +0530

    Environment configuration

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-var express= require('express');
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var express= require('./config/express');
 
 var app=express();
 
-app.use('/',function(req,res){
-	res.send("Hello first express app...YAY!!");
-})
-
 app.listen(3000);
+module.exports=app;
 
 console.log("Server running at http://localhost:3000/");
 
-module.exports=app;
